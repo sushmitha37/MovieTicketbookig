67889826cb24304bcd8e00b3f9ca72007db97fd5
SUSHMITHA-MOSALI1 <SUSHMITHA.MOSALI1@ibm.com>
2024-10-20 16:55:55 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       
       <Router>
         <Routes>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </Router>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,18 +3,17 @@ import {Link} from 'react-router-dom';
 function Signup() {
   return (
     <div id="box1">
-            <h2 id="name">ONE7</h2>
-
+        <h2 id="name">ONE7</h2>
      <form action="">
+        
         <input type="text" className="user" placeholder='firstName' required/>
         <input type="text" className="user" placeholder='lastName'  />
-        <input type="tel" className="user" placeholder='Mobile no'required/>
+        <input type="number" className="user" placeholder='Mobile no'required/>
         <input type="email" className="user" placeholder='Email'required/>       
         <input type="password" className="user" placeholder='password' required/>
         <input type="password" className="user" placeholder='confirm password' required/>
         <button className="user" id="btn">Signup</button>
-        <p>Have an account ? <Link to="/login">Login</Link></p>
-
+        <p>Have account ? <Link to="/">Login</Link></p>
 
         
         
