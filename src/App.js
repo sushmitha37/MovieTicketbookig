8d26c454a8d32a81bfb6e40280181bd813c0a4d0
SUSHMITHA-MOSALI1 <SUSHMITHA.MOSALI1@ibm.com>
2024-10-20 16:57:23 +0530

    semicolon added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Login from './components/Login.jsx'
+import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
