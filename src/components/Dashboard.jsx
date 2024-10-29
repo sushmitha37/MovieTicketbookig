7a26f980893e78a76f793188aa576f379d8e1a52
Suni17sunny37 <sunilkancharla0@gmail.com>
2024-10-29 09:01:49 +0530

    header added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div id="container">
-      <h1 id="dash-head">ONE7</h1>
-    </div>
+    <header id="navbar">
+      <div id="items">
+        <h1 >ONE7</h1>
+        <input type="text" placeholder='Search for Movies, Events, Sports and Entertainment'/>
+        <select name="location" id="dropdown">
+          <option value="Hyderabad">Hyderabad</option>
+          <option value="Chennai">Chennai</option>
+          <option value="Bangalore">Bangalore</option>
+          <option value="Kochi">Kochi</option>
+          <option value="Mumbai">Mumbai</option>
+          <option value="Kolkata">Kolkata</option>
+          <option value="Delhi">Delhi</option>
+          <option value="Pune">Pune</option>
+        </select>
+        <button>Sign in</button>
+      </div>
+    </header>
   )
 }
 
